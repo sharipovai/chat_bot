e59886d3fb91282586a86f7e7ed03896a2f557b1
Sharipov Artur <Sharipov@dedicorp.ru>
2025-10-31 06:26:31 +0300

    add config example

---
+++ config_example.py
@@ -0,0 +1,25 @@
+
+prod_bot_token = ""
+# test_bot_token = ""
+database_path = "chat_bot_db.sql"
+admin_tg_id = XXXX
+
+model_names = {'DeepSeek v3': "deepseek/deepseek-chat-v3-0324", 'DeepSeek r1': "deepseek/deepseek-r1:free",
+               "Qwen3 235b-a22b": "qwen/qwen3-235b-a22b:free", "Qwen3 32b": "qwen/qwen3-32b:free",
+               "Gemini-2.0-flash": "google/gemini-2.0-flash-001"}
+model_description = {'DeepSeek v3': "Быстрая, дата релиза 03.25", 'DeepSeek r1': "Умная, медленная, дата релиза 01.25",
+                    "Qwen3 235b-a22b": "Умная, медленная, дата релиза 04.25", "Qwen3 32b": "Быстрая, дата релиза 04.25",
+                    "Gemini-2.0-flash": "Быстрая, дата релиза 02.25"}
+
+
+question1 = "Составь краткое описание в одном предложении о человеке, спрашивающем: "
+question2 = "Если бы ты обладал памятью, ты бы записал эту информацию в кратковременную или долговременную память? " \
+            "ответь двумя словами"
+
+gemini_model = "google/gemini-2.0-flash-001"
+open_router_api_key = ""
+
+genai_api_key = ""
+
+
+
